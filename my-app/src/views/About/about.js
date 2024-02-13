264bbe8eaa99b4f105896f1275ebe4c04e5e522c
aymeric-bascunana <130646537+aymeric-bascunana@users.noreply.github.com>
2024-02-13 18:09:20 +0100

    modification du 13/02/2024

--- my-app/src/views/About/about.js
+++ my-app/src/views/About/about.js
@@ -8,10 +8,22 @@ function About() {
         <p className="bannerImg"></p>
       </section>
       <div className="container-collapse">
-        <Collapse title="Flabilité" content="Contenu du collapse Flabilité" />
-        <Collapse title="Respect" content="Contenu du collapse Respect" />
-        <Collapse title="Service" content="Contenu du collapse Service" />
-        <Collapse title="Sécurité" content="Contenu du collapse Sécurité" />
+        <Collapse
+          title="Fiabilité"
+          content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+        />
+        <Collapse
+          title="Respect"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+        />
+        <Collapse
+          title="Service"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+        />
+        <Collapse
+          title="Sécurité"
+          content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+        />
       </div>
     </>
   );
